37c975ee301830bcd9fac1f351e2d1830f058937
Setoville <a6seto@edu.uwaterloo.ca>
2019-11-23 15:29:03 -0500

    Add twilio capability

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const TOKEN_PATH = "./tokens.json";
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var tokens = require(TOKEN_PATH);
+const account_sid = tokens.sid;
+const auth_token = tokens.token;
+const client = require('twilio')(account_sid,auth_token);
+console.log(client);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    client.messages
+    .create({
+        body: 'happy thoughts test message',
+        from: tokens.src_phone,
+        to: tokens.dst_phone 
+    })
+    .then(message => console.log(message.sid));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
