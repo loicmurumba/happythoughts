51be75477e36e267c734bd72053a1914a8226fd7
Lauren <lauren.ferraro@outlook.com>
2019-11-24 06:02:44 -0500

    UI updates

--- happy-thoughts-react-app/src/Form.js
+++ happy-thoughts-react-app/src/Form.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import { Button } from "@material-ui/core";
-import { MuiPickersUtilsProvider, KeyboardTimePicker } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
 
 import TimeSliders from "./TimeSliders";
 import CountSlider from "./CountSlider";
@@ -110,7 +108,17 @@ class Form extends React.Component {
         <TimeSliders name="Sleep Reminders" lid="slide2" cid="time2" />
         <CountSlider name="Occasional Memes" lid="count1" cid="countId1" />
         <div className="submit">
-          <Button type="submit" variant="contained" color="primary" onClick={this.handleSubmit}>
+          <Button
+            style={{
+              backgroundColor: "#7130A1",
+              fontSize: "18px",
+              marginBottom: "24px"
+            }}
+            type="submit"
+            variant="contained"
+            color="primary"
+            onClick={this.handleSubmit}
+          >
             Save Settings
           </Button>
         </div>
