ff3811bb1c4e2f794c42fb9c75b6de4e73e3bc0e
Austin Kranc <austinkranc@rogers.com>
2019-11-24 04:54:55 -0500

    Server parse, push, pull - functionality created
    Server functionality. Twilio message responses. Receive and parse user messages. Pull and parse from database.

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.PORT || 8080; 
 
 const TOKEN_PATH = "./tokens.json";
+var responseType = 0; //0 text only
 
 var tokens = require(TOKEN_PATH);
+var lastMessage;
 const account_sid = tokens.sid;
 const auth_token = tokens.token;
 
@@ -30,10 +32,43 @@ const messaging_response = require('twilio').twiml.MessagingResponse;
 const client = require('twilio')(account_sid,auth_token);
 
 app.post('/sms',(req, res) => {
+	lastMessage = req; 
     const twiml = new messaging_response();
-    twiml.message('happy thought reply');
+	if(req.body.Body.includes("Hi") || req.body.Body.includes("Hey") || req.body.Body.includes("hi") || req.body.Body.includes("hey")){ 
+    twiml.message('Hey! :)\n\nText WOO for motivation\n\nText ZZZ for sleep\nText RLF for relief\nText HAHA for memes\n\nText INFO for more info\n\nThink Happy Thoughts :)');
+	}else if(req.body.Body.includes("WOO") || req.body.Body.includes("Woo") || req.body.Body.includes("woo")){
+		if(responseType == 0){
+		twiml.message("We May Encounter Many Defeats But We Must Not Be Defeated");	
+		}else{
+			twiml.message("GOOD MORNING!! :) https://sayingimages.com/wp-content/uploads/aaaahhh-heeeellloooo-and-good-morning-cute-memes.jpg");
+		}
+		
+	}else if(req.body.Body.includes("ZZZ") || req.body.Body.includes("Zzz") || req.body.Body.includes("zzz")){
+		if(responseType == 0){
+			twiml.message("Slow down, breathe, and let go of the stressful day");
+		}else{
+			twiml.message("Here's some meditation for you: https://www.youtube.com/watch?v=TP2gb2fSYXY");
+		}
+	}else if(req.body.Body.includes("RLF") || req.body.Body.includes("Rlf") || req.body.Body.includes("rlf")){
+		if(responseType == 0){
+			twiml.message("Right now clouds are covering the sky and everything seems dark and overcast. It is only a matter of time before the sun is out and everything is bright and hopeful.");
+		}else{
+			twiml.message("Right now clouds are covering the sky and everything seems dark and overcast. It is only a matter of time before the sun is out and everything is bright and hopeful.");
+		}
+	}else if(req.body.Body.includes("HAHA")){
+		if(responseType == 0){
+			twiml.message("What happened when the strawberry attempted to cross the road??\n\nThere was a traffic jam! :)");
+		}else{
+			twiml.message("Meme incoming!!! https://www.memesmonkey.com/images/memesmonkey/fc/fc30eca6199ffc303b722dde7d797df2.jpeg");
+		}
+	}else if(req.body.Body.includes("INFO")){
+		twiml.message("Please visit https://devpost.com/software/happy-thoughts-acqv95 for more info");
+	}else if(req.body.Body.includes("kill") || req.body.Body.includes("suicide") || req.body.Body.includes("jump") || req.body.Body.includes("end")){
+		twiml.message("Crisis Services Canada\nCALL: 1-(833)-456-4566\nTEXT: 'Start' to 45645\n\nUS National Suicide & Crisis Hotline\nCALL: 1-800-273-TALK (8255)\nTEXT: 'Hello' to 741741");
+	}
     res.writeHead(200,{'Content-Type':'text/xml'});
     res.end(twiml.toString());
+	
 });
 
 app.get('/', (req, res) => {
@@ -59,10 +94,33 @@ settingsRef.on('value', function(snapshot) {
     var snap = snapshot.val();
     var response = "";
     if (snap.type.includes("links")) {
-        
+        responseType = 1;
     }
 
     if (snap.type.includes("text")) {
-
+		responseType = 0;
     }
+	
+	if(snap.type.includes("schedule"){
+		if(responseType == 0){
+		client.messages
+		  .create({
+			 body: 'Slow down, breathe, and let go of the stressful day',
+			 from: '+12048195160',
+			 to: '+14152542499'
+		   })
+		  .then(message => console.log(message.sid));
+		}
+		
+		if(responseType == 1){
+		client.messages
+		  .create({
+			 body: "Here's some meditation for you: https://www.youtube.com/watch?v=TP2gb2fSYXY",
+			 from: '+12048195160',
+			 to: '+14152542499'
+		   })
+		  .then(message => console.log(message.sid));
+		}
+		
+	}
 });
